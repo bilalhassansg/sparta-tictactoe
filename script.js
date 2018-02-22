c1bf581d36a7e3e93e9bb62aef85bb2eeaa1e3c8
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-02-22 20:51:55 +0000

    include a start button; turns green when clicked.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-//get the players turn to be green
-$("h2").on('click', function(event){
-	$(this).css('color', 'green');
+$(function(){
+	var playerX = 1; // create variable player 1
+	var ttctable = $('.ttctable'); // create variable of the table
+	var message = $('h2').on(function(event) {
+		$(this).css('color', 'green');
+	})
+	displayNext(turn, player);
 
-});
+})
+
+$('#startgame').on('click', function(event) {
+	$(this).css('background', 'green');
+})
 
