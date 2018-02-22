01597ad485afed48488be3d00f15c38a6f472c70
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-02-22 22:51:29 +0000

    Get o after x and playero to change to blue

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 var playerX = 'X';
 var playerO = 'O'; // creating both players
+var next = '';
 var tabled = $('td'); // get td link from html page
 var playerturn = $('h2'); // get the player in play to display
 var reset = false;
+var restart =$('#reset')
 
 for (var i = 0; i < tabled.length; i++ ) {
-	$('td').on('click', function(event) {
+	$('td').on('click', function player1(event) {
 		$('.player1Turn').css('color', 'green');
 		$(this).html('X');
-	})
-}
+		//player2();
 
+		$('td').on('click', function player2(event) {
+			$('.player2turn').css('color', 'blue');
+			$(this).html('O');
+			return playerX;
+		})
+	});
+};
