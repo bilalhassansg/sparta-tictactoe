48d891c1620c52cc10776044c604345f4a5e085e
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-02-22 21:05:44 +0000

    get player in play to turn green

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var playerX = 'X';
 var playerO = 'O'; // creating both players
+var tabled = $('td'); // get td link from html page
+var playerturn = $('h2'); // get the player in play to display
+var reset = false;
 
-
-$('#startgame').on('click', function(event) {
-	$(this).css('background', 'green');
-	if(playerX == true){
-		$('td').on("click", function(event) {
-			$(this).html('X');
-		})
-	}
-})
+for (var i = 0; i < tabled.length; i++ ) {
+	$('td').on('click', function(event) {
+		$('.player1Turn').css('color', 'green');
+		$(this).html('X');
+	})
+}
 
