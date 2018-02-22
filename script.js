69965e82889f9fc3a7eac7c3c68e2654efe43d60
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-02-22 20:56:18 +0000

    create 2 players

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-$(function(){
-	var playerX = 1; // create variable player 1
-	var ttctable = $('.ttctable'); // create variable of the table
-	var message = $('h2').on(function(event) {
-		$(this).css('color', 'green');
-	})
-	displayNext(turn, player);
+var playerX = 'X';
+var playerO = 'O'; // creating both players
 
-})
 
 $('#startgame').on('click', function(event) {
 	$(this).css('background', 'green');
+	if(playerX == true){
+		$('td').on("click", function(event) {
+			$(this).html('X');
+		})
+	}
 })
 
